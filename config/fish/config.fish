930b03f254ad5b684fbf89f19d80252666fd4b6f
OmarNoob27 <omarahmed9113@gmail.com>
2021-12-03 18:27:39 +0200

    update my repo

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -385,13 +385,15 @@ end
 alias nnn='nnn -ed'
 alias n='nnn -ed'
 export NNN_PLUG='p:mocq;i:imgview;c:fzcd;t:mp3conv;e:suedit;v:preview-tui;V:nuke;j:autojump;x:!doas chmod +x $nnn;s:!bash -i*' #to play this plug press ; then choose option 
-export NNN_BMS="g:$HOME/Documents/Github/dotfiles;d:$HOME/Downloads/;h:~;s:~/scripts;f:~/ffmpeg;c:~/cell;w:~/wall;y:~/youtube-dl;t:~/.local/share/Trash/files;S:~/screenshots" #to play this plug press b then choose option 
+export NNN_BMS="g:$HOME/Documents/Github/dotfiles;d:$HOME/Downloads/;h:~;s:~/scripts;f:~/ffmpeg;c:~/cell;w:~/wall;y:~/youtube-dl;t:~/.local/share/Trash/files;S:~/screenshots;C:~/.config;p:~/pins" #to use this plug press B to add directory to bookmark and to play this plug press b then choose option 
 export NNN_OPENER=nnnopen
 export NNN_TMPFILE='/tmp/.lastd'
 #export NNN_COLORS='1267'
 export NNN_COLORS="2136"
-export NNN_FCOLORS='c1e26068E66033f7c6d6abc4'
+export NNN_FCOLORS='c1e26168E66033f7c6d6abc4'
 export NNN_FIFO=/tmp/nnn.fifo
 export NNN_TRASH=1 #to use trash (needs trash-cli) instead of delete. the trash's directory : /home/omar/.local/share/Trash/files
+#export NNN_ARCHIVE="\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)$"
+
 
 
