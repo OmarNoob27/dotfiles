e1a0e474904890935da6976e09c6ef73b461eee1
OmarNoob27 <omarahmed9113@gmail.com>
2022-02-07 16:11:01 +0200

    update my repos

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -371,6 +371,7 @@ alias smus='mpd && ncmpcpp'
 alias mus='ncmpcpp'
 alias emus='pkill mpd'
 alias rd='zaread'
+alias gsh='~/scripts/gsh'
 #search pkg with packman and install it 
 function pmsi
     pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro doas pacman -S
