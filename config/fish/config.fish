dd2ecade9094336e2683ea20c53c74bc0368eaec
OmarNoob27 <omarahmed9113@gmail.com>
2022-03-06 12:40:16 +0200

    update my repos

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -406,7 +406,7 @@ alias nn='nnn -eRx'
 alias n='nnn -Rxe'
 alias sn='sudo -E nnn -eRH'
 #export NNN_PLUG='p:mocq;i:imgview;c:fzcd;t:mp3conv;e:suedit;v:preview-tui;V:nuke;x:!doas chmod +x $nnn;k:pskill;K:!pkill mpv;s:!bash -i*;o:fzopen' #to play this plug press ; then choose option 
-export NNN_PLUG='m:nmount;i:imgview;f:fzcd;t:mp3conv;e:suedit;v:preview-tui;x:!doas chmod +x $nnn;k:pskill;s:!bash -i*;o:fzopen' #to play this plug press ; then choose option 
+export NNN_PLUG='m:nmount;i:imgview;f:fzcd;t:mp3conv;e:suedit;v:preview-tui;x:!doas chmod +x $nnn;s:!bash -i*;o:fzopen;k:kdeconnect' #to play this plug press ; then choose option 
 export NNN_BMS="g:$HOME/Documents/Github/dotfiles;d:$HOME/Downloads/;h:~;s:~/scripts;f:~/ffmpeg;C:~/cell;w:~/wall;y:~/youtube-dl;t:~/.local/share/Trash/files;S:~/screenshots;c:~/.config;p:~/pins;m:~/Music;e:~/enterprise;v:~/Videos;k:/run/user/1000/59cda44b9f5d1bb4/" #to play this plug press b then choose option 
 #xdg-open is the default opener for nnn 
 export NNN_OPENER=nnnopen #nnnopen path : /usr/bin/nnnopen
@@ -418,3 +418,4 @@ export NNN_FCOLORS='c1e29568E66033f7c6d6abc4'
 export NNN_FIFO=/tmp/nnn.fifo
 export NNN_TRASH=1 #to use trash (needs trash-cli) instead of delete. the trash's directory : /home/omar/.local/share/Trash/files
 export NNN_ARCHIVE="\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)"
+
